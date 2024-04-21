daa91b59b5b0d38de980dc17691c06e4627b362a
Kazzzemir <s1ckwell@mail.ru>
2024-04-21 17:42:28 +0300

    test1 done

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,21 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decode(e, t) {
+    const uniqueIds = new Set();
+    const decoded = 
+    e.map((obj) => {
+        for (let key in obj) {
+            if(t.hasOwnProperty(obj[key])) {
+                uniqueIds.add(obj[key])
+                obj[key] = t[obj[key]]
+            }
+        }
+        return obj;
+    })
 
+    return { uniqueIds: Array.from(uniqueIds), decoded }
+}
 
+const decoded = decode(encoded, translations);
 
-// console.log(decoded)
+console.log(decoded.decoded, decoded.uniqueIds);
